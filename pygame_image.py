811becee32e40b4e2c12d8b451b1d143a6ea3c05
kumagai <c0b23055af@edu.teu.ac.jp>
2024-11-19 15:52:17 +0900

    練習6-2

--- pygame_image.py
+++ pygame_image.py
@@ -13,11 +13,13 @@ def main():
     koukaton_img = pg.image.load("fig/3.png")
     koukaton_img = pg.transform.flip(koukaton_img,True,False)
     tmr = 0
+    x=0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
-
-        screen.blit(bg_img, [0, 0])
+            
+        x+=1
+        screen.blit(bg_img, [-(x%800), 0])
         screen.blit(koukaton_img, [300,200])
         pg.display.update()
         tmr += 1        
