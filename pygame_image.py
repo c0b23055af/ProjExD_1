c12b9e4b37303c99d10295fc7940e35d2bf02965
kumagai <c0b23055af@edu.teu.ac.jp>
2024-11-19 16:21:07 +0900

    練習8

--- pygame_image.py
+++ pygame_image.py
@@ -13,6 +13,8 @@ def main():
     flip_bg__img = pg.transform.flip(bg_img,True,False)
     koukaton_img = pg.image.load("fig/3.png")
     koukaton_img = pg.transform.flip(koukaton_img,True,False)
+    koukaton_rct = koukaton_img.get_rect()
+    koukaton_rct.center=300,200
     tmr = 0
     while True:
         for event in pg.event.get():
@@ -23,7 +25,16 @@ def main():
         screen.blit(flip_bg__img, [x+1600, 0])
         screen.blit(bg_img, [x+3200, 0])
         screen.blit(flip_bg__img, [x+4800, 0])
-        screen.blit(koukaton_img, [300,200])
+        screen.blit(koukaton_img, koukaton_rct)
+        key_lst=pg.key.get_pressed()
+        if key_lst[pg.K_UP]:
+            koukaton_rct.move_ip((0,-1))
+        if key_lst[pg.K_DOWN]:
+            koukaton_rct.move_ip((0,1))
+        if key_lst[pg.K_RIGHT]:
+            koukaton_rct.move_ip((1,0))
+        if key_lst[pg.K_LEFT]:
+            koukaton_rct.move_ip((-1,-0))
         pg.display.update()
         tmr += 1        
         clock.tick(200)
